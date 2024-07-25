e7757c1d86c73cb5c033d6a4cbbade11d566bc6b
Eugenio Miller <eugeniomiller93@gmail.com>
2024-07-25 10:57:08 -0300

    change db

--- app/Models/Register.php
+++ app/Models/Register.php
@@ -9,11 +9,6 @@ class Register extends Model
 {
     use HasFactory;
 
-     //Relation one to one with user
-     public function user(){
-        return $this->hasOne('App\Models\User');
-    }
-
     //Relation one to many with courses
     public function courses(){
         return $this->hasMany('App\Models\Course');
--- database/migrations/2024_07_24_171210_create_registers_table.php
+++ database/migrations/2024_07_24_171210_create_registers_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('registers', function (Blueprint $table) {
             $table->id();
-
-            //FK user
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name', 20);
+            $table->string('surname', 60);
+            $table->string('email')->unique();
 
             //FK courses
             $table->unsignedBigInteger('course_id');
